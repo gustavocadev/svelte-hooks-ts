03f45aeb419733302d7e2c6a7ffd8025ad696c0f
gustavocadev <churaalaygustavo11@gmail.com>
2023-02-20 16:50:42 -0500

    feat: github actions configured

--- dist/hooks/createCounter.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const createCounter: (initialValue?: number | undefined) => {
-    counter: {
-        subscribe: (this: void, run: import("svelte/store").Subscriber<number>, invalidate?: ((value?: number | undefined) => void) | undefined) => import("svelte/store").Unsubscriber;
-    };
-    increment: (by?: number) => void;
-    decrement: (by?: number) => void;
-    reset: () => void;
-};
