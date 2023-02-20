54d70419a4348dada6673a88bf3541bd58f6472d
gustavocadev <churaalaygustavo11@gmail.com>
2023-02-20 17:09:57 -0500

    style: show a message of counter example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,6 @@
 	const { increment, counter } = createCounter();
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Counter example</h2>
 <button on:click={() => increment()}>Click me</button>
 <span>Clicked {$counter} times</span>
